b77adfe1d0b9593c79321caf7646f60288c1df06
fencljon <91213738+fencljon@users.noreply.github.com>
2021-11-15 13:40:39 +0100

    Add files via upload

---
+++ StavUzlu.h
@@ -0,0 +1,11 @@
+#ifndef STAVUZLU_H_INCLUDED
+#define STAVUZLU_H_INCLUDED
+
+struct StavUzlu {
+    bool navstiven = false;
+    int predchudce_x;
+    int predchudce_y;
+};
+
+
+#endif // STAVUZLU_H_INCLUDED
